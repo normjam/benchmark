9178f9c939d83cc16bd5c2da677014853351d938
maniarathi <arathi.mani@chanzuckerberg.com>
2019-11-20 11:20:27 -0500

    Dump glmpca to loom.

--- methods/glmpca_method_runner.py
+++ methods/glmpca_method_runner.py
@@ -1,21 +1,23 @@
 import numpy as np
-import scanpy as sc
 from glmpca.glmpca import glmpca
+
 from ..method_runner import MethodRunner
 
 
 class GLMPCAMethodRunner(MethodRunner):
 
-    def __init__(self, adata, verbose, n_latent):
+    def __init__(self, data, verbose, n_latent):
         super(MethodRunner, self)
         self.n_latent = n_latent
-        self.adata = adata
 
     def run(self):
-        Y = self.adata.X.todense().T
+        Y = self.data.X.todense().T
         res = glmpca(Y, self.n_latent)
 
         # Normalized matrix
         norm = np.dot(res['factors'], res['loadings'].T)
         self.adata.obsm['X_norm'] = norm
         self.adata.obsm['X_emb'] = res['factors']
+
+        self.dump_to_loom("glmpca_normalized", norm, {}, {})
+        self.dump_to_loom("glmpca_latent", res['factors'], {}, {})
