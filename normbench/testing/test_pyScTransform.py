d028319d3d08b8f3d76c022970ffda65559843b0
LuckyMD <m.d.luecken@gmail.com>
2019-11-20 14:55:54 -0500

    created package, added pyScTransform, added testing

---
+++ normbench/testing/test_pyScTransform.py
@@ -0,0 +1,14 @@
+import scanpy as sc
+import numpy as np
+from benchmark.methods import ad2seurat as a2s
+
+def test_pyScTransform():
+    adata = sc.datasets.pbmc3k()
+
+    a2s.pyScTransform(adata)
+
+    # Test that it runs
+    assert 'normalized' in adata.layers
+
+    # Test functionality
+    assert np.isclose(adata.layers['normalized'][2,7], 0.69314718)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='normbench',
+      version='0.1',
+      description='Benchmarking normalization methods',
+      url='https://github.com/normjam/benchmark',
+      author='Normjam benchmark team',
+      author_email='',
+      packages=['normbench'],
+      zip_safe=False)
+
