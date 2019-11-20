e1543c8244f4327c90889026f71faa74ee4ede67
maniarathi <arathi.mani@chanzuckerberg.com>
2019-11-20 15:35:19 -0500

    Fix scvi

--- normbench/methods/__init__.py
+++ normbench/methods/__init__.py
@@ -1,4 +1,4 @@
+from .ad2seurat import pyScTransform
 from .glmpca_method_runner import GLMPCAMethodRunner
 from .lognorm_runner import LogNormalizationRunner
 from .scvi_method_runner import ScViMethodRunner
-from .ad2seurat import pyScTransform
