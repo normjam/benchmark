b4e0bbf925af77cb59878edbbeb63d50a45b5a66
Ambrose J. Carr <mail@ambrosejcarr.com>
2019-11-25 15:43:49 -0500

    reorder installation, install biocManager 3.10

--- packages.R
+++ packages.R
@@ -2,11 +2,12 @@ install.packages("Seurat")
 install.packages("devtools")
 
 library(devtools)
-devtools::install_github("rhondabacher/SCnorm")
-devtools::install_github("catavallejos/BASiCS", ref = "batches")
 
 install.packages(c("RcppEigen", "urltools", "Rtsne","BiocManager","robustbase"))
-
-devtools::install_github("hms-dbmi/pagoda2")
+BiocManager::install(version="3.10", update = TRUE, ask = FALSE)
 BiocManager::install(c("GO.db", "org.Hs.eg.db","org.Mm.eg.db", "pcaMethods","DESeq2","edgeR","BiocGenerics"), suppressUpdates=TRUE);
+
+devtools::install_github("rhondabacher/SCnorm")
+devtools::install_github("catavallejos/BASiCS", ref = "batches")
+devtools::install_github("hms-dbmi/pagoda2")
 devtools::install_github("hms-dbmi/conos", ref="master")
