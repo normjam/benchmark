c86d53f522ff01c34efe48ed0f1c7bfb0260d221
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-11-25 23:43:22 +0000

    Try to fenegle order

--- packages.R
+++ packages.R
@@ -2,11 +2,16 @@ install.packages("Seurat")
 install.packages("devtools")
 
 library(devtools)
+BiocManager::install(
+  c("GO.db", "org.Hs.eg.db","org.Mm.eg.db", "pcaMethods", "DESeq2", "edgeR", "BiocGenerics", "SCnorm", "BASiCS"),
+  version = "3.10",
+  update = TRUE,
+  ask = FALSE
+);
 devtools::install_github("rhondabacher/SCnorm")
 devtools::install_github("catavallejos/BASiCS", ref = "batches")
 
 install.packages(c("RcppEigen", "urltools", "Rtsne","BiocManager","robustbase"))
 
 devtools::install_github("hms-dbmi/pagoda2")
-BiocManager::install(c("GO.db", "org.Hs.eg.db","org.Mm.eg.db", "pcaMethods","DESeq2","edgeR","BiocGenerics"), suppressUpdates=TRUE);
 devtools::install_github("hms-dbmi/conos", ref="master")
