96c0e5029fdbc21bb429289a70ed420b263096cf
LuckyMD <m.d.luecken@gmail.com>
2019-12-02 23:22:29 +0100

    added anndata cleaning function to cast unsigned ints to int64 in downloaded data for R conversion

--- normbench/methods/data.py
+++ normbench/methods/data.py
@@ -1,6 +1,7 @@
 import requests
 from io import BytesIO
 from anndata import AnnData, read_h5ad
+import numpy as np
 
 
 def _download_adata(url) -> AnnData:
@@ -9,17 +10,34 @@ def _download_adata(url) -> AnnData:
     return read_h5ad(f)
 
 
+# Just cast unsigned ints to int64
+# Hope that this doesn't break (uint64)
+def _no_unsigned_int(pdata):
+    for i,t in enumerate(pdata.dtypes):
+        if t.name.startswith('u'): #this is an unsigned integer
+            colname = pdata.columns[i]
+            pdata[colname] = pdata[colname].astype(int)
+
+
+# Helper function to clean anndata object
+def clean_adata(adata) -> AnnData:
+    _no_unsigned_int(adata.var)
+    _no_unsigned_int(adata.obs)
+    return(adata)
+    
 def pbmc3k() -> AnnData:
-    return _download_adata("https://ndownloader.figshare.com/files/18789473?private_link=6e12f5f56bb46842f5ee")
+    return clean_adata(_download_adata("https://ndownloader.figshare.com/files/18789473?private_link=6e12f5f56bb46842f5ee"))
 
 
 def cite_seq_bone_marrow() -> AnnData:
-    return _download_adata("https://ndownloader.figshare.com/files/18788633?private_link=cfbc86f6a399343677ea")
+    return clean_adata(_download_adata("https://ndownloader.figshare.com/files/18788633?private_link=cfbc86f6a399343677ea"))
 
 
 def endoderm_full() -> AnnData:
-    return _download_adata("https://ndownloader.figshare.com/files/18789569?private_link=fe99143697ea99121240")
+    return clean_adata(_download_adata("https://ndownloader.figshare.com/files/18789569?private_link=fe99143697ea99121240"))
 
 
 def endoderm_downsampled() -> AnnData:
-    return _download_adata("https://ndownloader.figshare.com/files/18788645?private_link=c204122a8d0550282502")
+    return clean_adata(_download_adata("https://ndownloader.figshare.com/files/18788645?private_link=c204122a8d0550282502"))
+
+
